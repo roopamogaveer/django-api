83206fea7364fb5b665f7012e77802bf575e2f97
Prasad Poojary <prasadspoojar@gmail.com>
2023-10-21 03:16:35 +0530

    integrated backend django with angular frontend and fixed minor bugs

---
+++ testui/src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User 
+{
+    public username:string;
+    public  email:string;
+    public password:string;
+
+    constructor()
+    {
+        this.username="";
+        this.email="";
+        this.password="";
+    }
+}
